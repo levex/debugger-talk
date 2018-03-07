20214fe4cb075db0902cc139b831e0c81f25c787
Levente Kurusa <levex@linux.com>
2018-03-07 13:58:06 +0000

    test_program: add a test program
    Signed-off-by: Levente Kurusa <levex@linux.com>

---
+++ test_program/main.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+static int target;
+
+void
+game(void)
+{
+    target = rand() % 100;
+
+    while(1) {
+        int guess, n;
+        printf("Make a guess: ");
+        n = scanf("%d", &guess);
+        if (n <= 0)
+            break;
+
+        if (guess > target) {
+            printf("It's lower!\n");
+        } else if (guess < target) {
+            printf("It's higher!\n");
+        } else {
+            printf("You've got it!\n");
+            break;
+        }
+    }
+}
+
+int
+main(int argc, char **argv)
+{
+    srand(time(NULL));
+
+    printf("Hello, SCALE 18x!\n");
+
+    game();
+
+    return 0;
+}
